74a80182259ff226deed4b77eae2bf34ec4460c3
DieterBohlen997 <37517811+DieterBohlen997@users.noreply.github.com>
2021-06-24 16:31:57 +0200

    Update jenkinsfile1.groovy

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -11,7 +11,7 @@ pipeline{
           println('asdasdasdasd')
           def helper = {
             Job job = currentBuild.rawBuild.parent
-            println(job.definition.scm.getAuthorOrCommiter().toString())
+            println(job.definition.scm.declaredMethods.findAll { !it.synthetic }.name    //getAuthorOrCommiter().toString())
          }
         helper()
       }
