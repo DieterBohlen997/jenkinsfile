0245d459015c884610d90c6c47d2e5b021391e45
DieterBohlen997 <37517811+DieterBohlen997@users.noreply.github.com>
2021-06-24 16:05:52 +0200

    Update jenkinsfile1.groovy

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -11,7 +11,7 @@ pipeline{
           println('asdasdasdasd')
           def helper = {
             Job job = currentBuild.rawBuild.parent
-            println(job.definition.scm.branches.getClass().getDeclaredFields().getDeclaredFields())
+            println(job.definition.scm)
          }
         helper()
       }
