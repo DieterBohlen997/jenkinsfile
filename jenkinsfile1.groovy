bf0b1692754b67d4037e6e260cc22853b5b02ec5
DieterBohlen997 <37517811+DieterBohlen997@users.noreply.github.com>
2021-06-24 16:08:53 +0200

    Update jenkinsfile1.groovy

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -11,7 +11,7 @@ pipeline{
           println('asdasdasdasd')
           def helper = {
             //Job job = currentBuild.rawBuild.parent
-            println(job.definition.scm.branches)
+            println(Job.definition.scm.branches)
          }
         helper()
       }
