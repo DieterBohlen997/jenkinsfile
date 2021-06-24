5480f5f1e85ebaa7a19df718ffb7fff517a49f27
DieterBohlen997 <37517811+DieterBohlen997@users.noreply.github.com>
2021-06-24 16:28:02 +0200

    Update jenkinsfile1.groovy

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -11,7 +11,7 @@ pipeline{
           println('asdasdasdasd')
           def helper = {
             Job job = currentBuild.rawBuild.parent
-            println(job.definition.scm.getMethods().toString())
+            println(job.definition.scm.getAuthorOrCommiter().toString())
          }
         helper()
       }
